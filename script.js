8db14add782719390625530fe7a28a9a207525a4
AlexTeRad <a.prach99@gmail.com>
2023-08-29 19:07:31 +0300

    Update

--- script.js
+++ script.js
@@ -1,186 +1,133 @@
-// Запускаем функцию, когда DOM полностью загружен
-// Идет перечисление функций slick
-$(document).ready(() => {
-  // Инициализируем слайдер с определенными настройками
-  $(".slider").slick({
-    dots: false, // Не показывать точки под слайдером
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-  });
-  // Устанавливаем максимальную высоту слайдов
-  const maxHeight = Math.max(
-    300,
-    ...$(".slider__screen")
-      .map(function () {
-        return $(this).height();
-      })
-      .get()
-  );
-  $(".slider__screen").height(maxHeight);
-});
+// Итак господа , я придумал домашку.
+// Задача 1
+// Представьте себе, что у вас 2 ведра разных размеров - 5л и 3л.
+// Вам нужно отмерить 4л. Эту задачу нужно сделать с помощью нескольких функций или обьектов с помощью замыкания нового значения воды в ведре.
+//  Задача 2
+// Представьте себе, что у вас 2 команды по пляжному волейболу по 3 человека в каждой, но на поле каждой из команд - 4 зоны. Соответственно когда-то мяч попадет на зону, которая команда не закрыла и очко засчитается команде противника.
+// Сделать нужно через цикл, команды закрывают 3 из 4 зон - и после каждого перехода они меняются (рендомно) - тут нужна функция-рендомайзер и должен быть счетчик перехода меча от команды к комманде пока мяч не попадет в неприкрытую зону и счетчик матча, к примеру , кто выгрывает 3 раунда - выигрывает игру
 
-// Текущий активный слой фона
-let activeLayer = 1;
-// Функция изменения фона в зависимости от текущего слайда
-const changeBackground = (currentSlide) => {
-  // Получаем два элемента фона
-  const [layer1, layer2] = [
-    document.getElementById("background-layer-1"),
-    document.getElementById("background-layer-2"),
-  ];
-  // Определение градиента для фона на основе текущего слайда
-  const gradient =
-    [
-      "linear-gradient(to right, #020024, #00d4ff)",
-      "linear-gradient(to right, #FFB6C1, #FF69B4)",
-      "linear-gradient(to right, #22c1c3, #fdbb2d)",
-      "linear-gradient(to right, #eeaeca, #94bbe9)",
-    ][currentSlide] || "";
-  // Переключаем фоны и их прозрачность
-  if (activeLayer === 1) {
-    layer2.style.background = gradient;
-    [layer1.style.opacity, layer2.style.opacity, activeLayer] = [0, 1, 2];
-  } else {
-    layer1.style.background = gradient;
-    [layer1.style.opacity, layer2.style.opacity, activeLayer] = [1, 0, 1];
-  }
+//*Задача №1
+// Функция для создания ведра с заданной емкостью
+const createBucket = (size) => {
+  // Локальная переменная, которая хранит текущий объем воды в ведре.
+  // Начальное значение установлено в 0.
+  let currentAmount = 0;
+  // Возвращаем объект, представляющий ведро
+  return {
+    // Метод для наполнения ведра до его максимальной емкости
+    fill: () => {
+      currentAmount = size;
+    },
+    // Метод для опустошения ведра
+    empty: () => {
+      currentAmount = 0;
+    },
+    // Метод для переливания воды из текущего ведра в другое
+    pourInto: (otherBucket) => {
+      // Рассчитываем, сколько свободного места осталось в другом ведре
+      const spaceInOtherBucket =
+        otherBucket.getCapacity() - otherBucket.getCurrentAmount();
+      // Определяем, сколько воды мы можем перелить (меньшее из двух значений: текущего объема или свободного места в другом ведре)
+      const amountToPour = Math.min(currentAmount, spaceInOtherBucket);
+      // Добавляем воду в другое ведро
+      otherBucket.add(amountToPour);
+      // Уменьшаем объем воды в текущем ведре
+      currentAmount -= amountToPour;
+    },
+    // Метод для получения текущего объема воды в ведре
+    getCurrentAmount: () => currentAmount,
+    // Метод для получения максимальной емкости ведра
+    getCapacity: () => size,
+    // Метод для добавления указанного объема воды в ведро
+    add: (amount) => {
+      currentAmount += amount;
+      // Если объем воды превышает емкость ведра, установите его равным емкости ведра
+      if (currentAmount > size) {
+        currentAmount = size;
+      }
+    },
+  };
 };
-// Слушатель событий для смены фона при изменении слайда
-$(".slider").on("afterChange", (_, __, currentSlide) =>
-  changeBackground(currentSlide)
-);
-// Переход к следующему слайду
-goToNextSlide = () => {
-  $(".slider").slick("slickNext");
-};
-// Начало работы с формой
-// Функция для сохранения данных
-const saveData = () => {
-  // Поля для сбора данных
-  const fields = [ //Массив с индефикаторами
-    "email",
-    "login",
-    "password",
-    "birthdate",
-    "gender",
-    "name",
-    "zodiac",
-    "weight",
-    "height",
-  ];
-  // Сбор данных из полей в объект person
-  const person = fields.reduce(
-    (obj, field) => ({ ...obj, [field]: $("#" + field).val() }),
-    {}
-  );
-  // Проверка заполненности всех полей
-  if (Object.values(person).every((value) => value)) {
-    //Использую Object.values(person), чтобы получить массив всех значений объекта person. Затем с помощью метода every проверяю, что каждое значение не является пустым.
-    // Если все поля заполнены, то сохраняем данные в локальном хранилище
-    const people = [
-      ...JSON.parse(localStorage.getItem("people") || "[]"), // Используем ... для извлечения массива
-      person, //добавляю в массив новый объект
-    ];
-    localStorage.setItem("people", JSON.stringify(people)); // Загружаю обратно в локальное хранилище предварительно переведя массив в строку
-    // Отображаем результаты и переходим к следующему слайду
-    showResults();
-    $(".slider").slick("slickNext");
-    alert("Все записано!");
-  } else {
-    // Если не все поля заполнены, то выводит сообщение об ошибке
-    alert("Пожалуйста, заполните все поля!");
-  }
-};
-// Показ результатов при загрузке страницы
-document.addEventListener( //Запускаю слушателя который будет реагировать на дом когда html будет загружен. без него не хотелось отображаться
-  "DOMContentLoaded",
-  (showResults = () => {
-    // Получаем данные из локального хранилища
-    const people = JSON.parse(localStorage.getItem("people") || "[]");
-    // Вычисляем результаты
-    let tallest = getTallestPerson(people);
-    let heaviest = getHeaviestPerson(people);
-    let genderCounts = getGenderCounts(people);
-    let uniqueNames = getUniqueNames(people);
-    let totalWeight = getTotalWeight(people);
-    // Очищаем предыдущие результаты
-    const resultContainer = document.getElementById("result");
-    resultContainer.innerHTML = "";
-    // Отображаем результаты на странице
-    appendInfoElement(
-      `Самый высокий: ${tallest.name}, рост: ${tallest.height} см`
-    );
-    appendInfoElement(
-      `Самый толстый: ${heaviest.name}, вес: ${heaviest.weight} кг`
-    );
-    appendInfoElement(
-      `Мальчиков: ${genderCounts.male}, Девочек: ${genderCounts.female}, Других: ${genderCounts.other}`
-    );
-    appendInfoElement(`Совпадение по имени: ${uniqueNames.join(", ")}`);
-    appendInfoElement(`Общий вес ${totalWeight} кг`);
-  })
-);
-// Создание элемента для отображения информации
-const createInfoElement = (text) => {
-  const infoElement = document.createElement("div");
-  infoElement.textContent = text;
-  infoElement.classList.add("result__div");// Добавляем класс
-  return infoElement;
-};
-// Добавление элемента с информацией в результат
-const appendInfoElement = (text) => {
-  const resultContainer = document.getElementById("result");
-  const infoElement = createInfoElement(text);
-  resultContainer.appendChild(infoElement);
-};
-// Функции для обработки данных
 
-// Рост
-const getTallestPerson = (people) => {
-  return people.reduce(
-    //tallest акумуляторное значение а person представляет текущий элемент массива
-    (tallest, person) => (person.height > tallest.height ? person : tallest) // здесь мы сравниваем самого высокого человека на текущий моммент и поступивщего
-  );
+const measureFourLiters = () => {
+  const bucket5 = createBucket(5);
+  const bucket3 = createBucket(3);
+
+  // Шаги для отмеривания 4л:
+  // 1. Наполняем 5л ведро
+  // 2. Выливаем из 5л ведра в 3л ведро
+  // 3. Опустошаем 3л ведро
+  // 4. Выливаем оставшиеся 2л из 5л ведра в 3л ведро
+  // 5. Наполняем 5л ведро
+  // 6. Выливаем из 5л ведра в 3л ведро (в котором уже есть 2л)
+
+  bucket5.fill();
+  bucket5.pourInto(bucket3);
+  bucket3.empty();
+  bucket5.pourInto(bucket3);
+  bucket5.fill();
+  bucket5.pourInto(bucket3);
+
+  return bucket5.getCurrentAmount();
 };
-// Вес
-const getHeaviestPerson = (people) => {
-  return people.reduce((heaviest, person) =>
-    person.weight > heaviest.weight ? person : heaviest
-  );
+
+console.log(measureFourLiters()); // Выведет: 4
+
+//*Задача №2
+// Функция для получения случайного числа от 1 до 4
+const getRandomZone = () => Math.floor(Math.random() * 4) + 1;
+// Функция для рандомного выбора 3 покрытых зон командой
+const getCoveredZones = () => {
+  const zones = [1, 2, 3, 4];
+  const coveredZones = [];
+
+  while (coveredZones.length < 3) {
+    const index = Math.floor(Math.random() * zones.length);
+    coveredZones.push(...zones.splice(index, 1)); // Извлекаем случайную зону и добавляем её в покрытые зоны
+  }
+
+  return coveredZones;
 };
-// Пол
-const getGenderCounts = (people) => {
-  const genderOptions = ["мужской", "женский", "другой"];
-  let genderCounts = { male: 0, female: 0, other: 0 };
-  people.forEach((person) => {
-    switch (person.gender) {
-      case genderOptions[0]:
-        genderCounts.male++;
-        break;
-      case genderOptions[1]:
-        genderCounts.female++;
-        break;
-      case genderOptions[2]:
-        genderCounts.other++;
-        break;
+// Основная логика игры
+const playGame = () => {
+  let teamAScore = 0;
+  let teamBScore = 0;
+
+  while (teamAScore < 3 && teamBScore < 3) {
+    // Команда A выбирает покрытые зоны
+    let teamACoveredZones = getCoveredZones();
+    // Мяч направляется в случайную зону
+    let ballZone = getRandomZone();
+    // Если мяч попал в непокрытую зону командой A
+    if (!teamACoveredZones.includes(ballZone)) {
+      teamBScore++;
+      console.log(
+        `Round ${
+          teamAScore + teamBScore
+        }: Team B scores! Current score: Team A - ${teamAScore}, Team B - ${teamBScore}`
+      );
+      continue; // Переходим к следующему раунду
     }
-  });
-  return genderCounts;
+    // Команда B выбирает покрытые зоны
+    let teamBCoveredZones = getCoveredZones();
+    // Мяч направляется в случайную зону
+    ballZone = getRandomZone();
+    // Если мяч попал в непокрытую зону командой B
+    if (!teamBCoveredZones.includes(ballZone)) {
+      teamAScore++;
+      console.log(
+        `Round ${
+          teamAScore + teamBScore
+        }: Team A scores! Current score: Team A - ${teamAScore}, Team B - ${teamBScore}`
+      );
+    }
+  }
+  // Выводим победившую команду
+  if (teamAScore === 3) {
+    console.log("Team A wins the game!");
+  } else {
+    console.log("Team B wins the game!");
+  }
 };
-
-// Общий вес
-const getTotalWeight = (people) => {
-  return people.reduce((totalWeight, person) => totalWeight + person.weight, 0);
-};
-
-// Уникальность имени
-const getUniqueNames = (people) => {
-  return people
-    .map((person) => person.name)// Создаем новый массив только с именами
-    .filter((name, i, a) => a.indexOf(name) === i && a.lastIndexOf(name) !== i);
-    //Применяем метод filter для поиска уникальных имен. name- это имя в массиве, i - его индекс a-сам массив. 
-};
-
-// Получаем список людей из локального хранилища или пустой массив, если данных нет
-let people = JSON.parse(localStorage.getItem("people")) || [];
+// Запускаем игру
+playGame();
